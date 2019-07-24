9c4dbd3de857a82b655e50ec8914d8145f745ac9
Feevah <cddefever@gmail.com>
2019-07-24 14:57:02 -0400

    getRequest

---
+++ ajaserver.js
@@ -0,0 +1,34 @@
+console.log("happy")
+
+
+
+var http = require("http")
+const http = require ('http');
+const fs = require ('fs');
+
+
+
+
+
+
+const server = http.createServer(function (req, res){
+const url = req.url;
+const method = req.method;
+
+
+
+//here
+
+
+
+});
+
+
+server.listen(8080, function(){
+	console.log ("listening on port: 8080")
+
+}); 
+
+
+
+
